5ad6be068fe5212b962fb496d74b476adf6e8418
Yash Karakoti <karakotiyash01@gmail.com>
2025-05-03 18:57:35 +0530

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,19 +15,6 @@ const config: Config = {
         "theme-dark-bg": "#1a202c",
         "theme-light-bg": "#ffffff",
       },
-      dark: {
-        css: {
-          color: theme("colors.gray.200"),
-          a: { color: theme("colors.blue.400") },
-          strong: { color: theme("colors.gray.100") },
-          code: { color: theme("colors.purple.300") },
-          blockquote: { color: theme("colors.gray.300") },
-          h1: { color: theme("colors.white") },
-          h2: { color: theme("colors.white") },
-          h3: { color: theme("colors.white") },
-          h4: { color: theme("colors.white") },
-        },
-      },
     },
   },
   plugins: [typography], // Use the imported typography plugin
